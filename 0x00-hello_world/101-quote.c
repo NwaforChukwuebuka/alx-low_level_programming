790b2929b7502dc92637f1fad395844de1d6c6f7
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2023-10-05 15:05:18 -0700

    C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	const char* text = "and that piece of art is useful\"\
+			    - Dora Korpar, 2015-10-19";
+	write(2, text, strlen(text));
+	return (1);
+}
+
