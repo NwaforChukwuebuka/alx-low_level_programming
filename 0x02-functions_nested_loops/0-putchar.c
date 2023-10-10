b63c5765cd4791495db0527e39679788810a7de8
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2023-10-10 13:22:56 -0700

    Single char printing using write(stdout, char, byte_num)

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
+
+int main (void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r);
+
+	return (0);
+}
