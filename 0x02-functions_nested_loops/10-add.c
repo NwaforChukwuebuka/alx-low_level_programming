8596f95b1ca2c228ccb28c7521d72e3b1f7ef981
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2023-10-10 19:30:09 -0700

    adds 2 int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers
+ * @n1: first int to be added
+ * @n2: second int to be added
+ *
+ * Return: The sum of the two int
+ *
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
