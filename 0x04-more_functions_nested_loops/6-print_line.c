b199ada624dac301a5b21e683245d5dda73e9241
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2023-10-12 18:43:04 -0700

    draws a straight line in the terminal.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * print_line - Draws a straight line according to parameter
+  * @num: The number of lines to draw
+  *
+  * Return: empty
+  */
+void print_line(int num)
+{
+	int i;
+
+	if (num <= 0)
+	{
+		_putchar('\n);
+	}
+	else
+	{
+		for (i = 0; i < num; i++)
+		{
+			_putchar(95);
+		}
+
+		_putchar('\n');
+	}
+}
