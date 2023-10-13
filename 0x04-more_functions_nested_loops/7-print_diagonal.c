c30bbf86e383785043516469253091f41533ae91
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2023-10-12 19:08:20 -0700

    Draws line on the terminal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * print_diagonal - Draws a diagonal lines according parameter
+  * @num: The number of times to print diagonal lines
+  *
+  * Return: empty
+  */
+void print_diagonal(int num)
+{
+	int i, j;
+
+	if (num <= 0)
+	{
+		_putchar('\n);
+	}
+	else
+	{
+		for (i = 0; i < num; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(32);
+			}
+
+			_putchar(92);
+			_putchar('\n);
+		}
+	}
+}
