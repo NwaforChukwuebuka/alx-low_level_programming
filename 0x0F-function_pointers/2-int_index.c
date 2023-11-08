6819c7e139e68888b8165670eda8c52dc2005472
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2023-11-08 15:15:30 -0800

    Argument function pointer taking elements of the arg array as input and comaparing in its function

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,10 +15,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size < 1 || array == NULL || cmp == NULL)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	while (i <= size)
 	{
 		if (cmp(array[i]))
+		{
 			return (i);
+		}
+		i++;
+
 	}
 
 	return (-1);
