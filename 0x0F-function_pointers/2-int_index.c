6f105c680177f553a190d891b534732039d15795
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2023-11-08 14:57:39 -0800

    Argument function pointer taking elements of the arg array as input and comaparing in its function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Function that searches for an integer
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: function pointer
+ * Return: index of first element that matches with `cmp`, or -1 if none found
+ */
+
+int int_index(int *array, size_t size, int (*cmp)(int))
+{
+	size_t i;
+
+	if (!array || !cmp || size < 1)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
