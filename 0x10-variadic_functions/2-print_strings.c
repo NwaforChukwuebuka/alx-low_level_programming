9f88f98032b3989989df20f5a1714855e758a36d
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2023-11-09 14:48:02 -0800

    print string separated using a sepaarator or delimiter

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_strings - prints string arguments with a separator
+ *
+ * @separator: - thing to print between strings
+ * @n: - number of arguments
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	char *sep;
+	unsigned int i;
+	char *ptr;
+
+	va_start(args, n);
+
+	if (separator == NULL || separator == 0)
+		sep = " ";
+	else
+		sep = (char *)separator;
+
+	if (n > 0)
+		printf("%s", va_arg(args, char *));
+
+	for (i = 1; i < n; i++)
+	{
+		ptr = va_arg(args, char *);
+		if (ptr == NULL)
+			ptr = "(nil)";
+
+		printf("%s%s\n", sep, ptr);
+	}
+}
