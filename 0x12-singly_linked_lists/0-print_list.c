7d4dac15cfbb0312f4dfa494088098ad8bee095a
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2023-11-16 06:48:11 -0800

    iterates through a linked list and returns the number of nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * print_list - print the data in the nodes of the singly linked list
+ * @h: a pointer to the head of the node
+ * Return: the size of the linked list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		printf("[%d] %s\n",h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
