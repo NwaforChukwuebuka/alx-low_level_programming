8f4a8cd22544727c99bc6c11a590012c41719c64
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2023-11-16 19:53:47 -0800

    a functions that runs before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * pre_main -runs before main
+ *
+ */
+
+void pre_main(void) __attribute__ ((constructor));
+
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
