5832eec4e8cb8ad61d2383f3f1c42f2086399277
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2023-11-16 18:49:41 -0800

    add node to the end of the linked list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node_end - adds a new node
+ * @head: ptr to a ptr to the head
+ * @str: string to duplicate into a new node
+ * Return: pointer to list_t struct
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *node;
+	list_t *p;
+
+	node = malloc(sizeof(list_t));
+	node->str = strdup(str);
+	node->len = strlen(str);
+
+	if (node->str == NULL)
+	{
+		return (NULL);
+	}
+
+	if (node == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		*head = node;
+		return (*head);
+	}
+	p = *head;
+	while (p->next != NULL)
+	{
+		p = p->next;
+	}
+	p->next = node;
+	return (*head);
+}
