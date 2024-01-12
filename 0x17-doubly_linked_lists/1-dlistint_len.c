ac3f7d380264eb98203cf98fb503b3fe4d773fd1
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-11 18:44:32 -0800

    betty compliant

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,11 @@
 #include "lists.h"
+
 /**
-  * dlistint_len – prints the length of the list
-  * @h: ptr to head of the list
+  * dlistint_len - Prints length of the list
+  * @h: Ptr to the head of the list
   * Return: Number of nodes in the linked list
   */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t nodeCnt;
