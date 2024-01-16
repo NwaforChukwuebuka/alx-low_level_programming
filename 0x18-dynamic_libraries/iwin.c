cacdb0b53102c720eed6ab03b74c09ec98c36891
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-01-15 20:01:14 -0800

    hacking casino

---
+++ 0x18-dynamic_libraries/iwin.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <string.h>
+
+int rand()
+{
+	static int count = -1;
+
+	count++;
+	if (count == 0)
+		return (8);
+	if (count == 1)
+		return (8);
+	if (count == 2)
+		return (7);
+	if (count == 3)
+		return (9);
+	if (count == 4)
+		return (23);
+	if (count == 5)
+		return (74);
+	return (count * count % 30000);
+}
