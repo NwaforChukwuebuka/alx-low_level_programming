320239dfcfae2d0e70fb79b66b59ca6f00dd2869
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-02-02 17:38:15 -0800

    get the index of the hash node

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,21 @@
+/*
+ * File: 2-key_index.c
+ * Auth: Nwafor Chukwuebuka
+*/
+
+#include "hash_tables.h"
+
+/**
+ * key_index – The index to find from the array
+ *             stored in array of a hash table.
+ * @key: Given key
+ * @size: The size of the array of the hash table.
+ *
+ * Return: The index of the key.
+ *
+ * Description: Uses the djb2 algorithm.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
