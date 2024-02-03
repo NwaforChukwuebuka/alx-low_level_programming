2470b802db7bdf9a282aa8e82a6ad90c68ef9330
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-02-02 17:40:50 -0800

    Betty compliant

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,15 +6,15 @@
 #include "hash_tables.h"
 
 /**
- * key_index – The index to find from the array
+ * key_index - The index to find from the array
  *             stored in array of a hash table.
  * @key: Given key
  * @size: The size of the array of the hash table.
  *
  * Return: The index of the key.
  *
- * Description: Uses the djb2 algorithm.
- */
+*/
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
