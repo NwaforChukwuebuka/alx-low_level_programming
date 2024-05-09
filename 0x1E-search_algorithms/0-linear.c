08c48f48c337d0edd66a43ae75781d0b440e29c5
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-05-08 19:58:28 -0700

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,9 +1,3 @@
-
-/*
- * File: 0-linear.c
- * Auth: Nwafor Chukwuebuka
- */
-
 #include "search_algos.h"
 
 /**
@@ -14,7 +8,7 @@
   * @value: The value to search for.
   *
   * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the first index where the value is located.
+  *         else, the first index where the value is located.
   *
   * Description: Prints a value every time it is compared in the array.
   */
@@ -27,11 +21,9 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%lu = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-		{
 			return (i);
-		}
 	}
 
 	return (-1);
